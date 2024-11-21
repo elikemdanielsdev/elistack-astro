e6b14aed4b9b6af6515d748295355273f1490e10
elikemdanielsdev <elikemdaniels@gmail.com>
2024-11-21 13:01:43 +0000

    General update

---
+++ src/utils/formattedDate.ts
@@ -0,0 +1,8 @@
+// Formatted Date
+export const formattedDate = (date: string) => {
+  return new Date(date).toLocaleDateString("en-US", {
+    year: "numeric",
+    month: "long",
+    day: "numeric",
+  });
+};
