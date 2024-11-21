7d949bb7c715956e980dbfae6f8ed98a73d9eeb9
elikemdanielsdev <elikemdaniels@gmail.com>
2024-11-21 06:50:18 +0000

    General update

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,7 +8,7 @@ import react from "@astrojs/react";
 // https://astro.build/config
 export default defineConfig({
   image: {
-    domains: ["elikem-directus.up.railway.app"],
+    domains: ["elikem-directus.up.railway.app", "images.unsplash.com"],
   },
   integrations: [tailwind(), react()],
 });
